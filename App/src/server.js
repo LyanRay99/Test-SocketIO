0ce342532d0464b33dae5b83e183f834b2b90be8
Tai Marce <mttai161099@gmail.com>
2023-03-21 12:46:56 +0700

    config & test socketIO

--- App/src/server.js
+++ App/src/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 const path = require("path");
 const http = require("http");
-const socketIO = require("socket.io");
+const { Server } = require("socket.io");
 
 app.use(express.json());
 
@@ -14,7 +14,29 @@ app.use(express.static(path.join(__dirname, "../public")));
 const server = http.createServer(app);
 
 //* declare socket io
-const IO = socketIO(server);
+const IO = new Server(server);
+
+// variables to test socketIO server
+let message = "Hello, world!";
+
+//* listen connection event from client
+IO.on("connection", (socket) => {
+  console.log("connected");
+
+  //* disconnect socket
+  socket.on("disconnect", () => {
+    console.log("disconnected");
+  });
+
+  //* send message
+  socket.emit("sendMessage", message);
+
+  //* give message from client, update and response
+  socket.on("increment", () => {
+    message = "this's message";
+    socket.emit("sendMessage", message);
+  });
+});
 
 server.listen(port, () => {
   console.log(`Server is running on port ${port}`);
